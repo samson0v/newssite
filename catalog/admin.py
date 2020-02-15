91805ff0cb0d905a6a90d0aa561f83980fdb72fd
samson0v <vitalikbidochka@gmail.com>
2020-02-15 16:44:06 +0200

    Custom admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Comment
 
-admin.site.register(News)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('News Description', {'fields': ['title', 'description', 'image']}),
+        ('Moderated', {'fields': ['moderated']}),
+        ('Author', {'fields': ['author']})
+    ]
+
+    inlines = [CommentInline]
+
+    list_display = ('title', 'author', 'moderated')
